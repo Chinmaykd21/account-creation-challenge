b2a3b02b58c113122b70bf888e47674d0281f943
chinmaykd21 <chinmaykd21@gmail.com>
2024-09-09 19:57:00 -0400

    add controller method to handle post request, and validate data before storing to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,26 @@ class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create_account]
   
   def create_account
-    ### This is where we will add logic to handle account creation
+    username = params[:username]
+    password = params[:password]
+    honeypot = params[:honeypot] || ''
+
+    # Validate honeypot field (should be empty)
+    if honeypot.length != 0
+      render json: { error: 'Bot detected. Submission will not be processed' }, status: :unprocessable_entity
+      return
+    end
+
+    begin
+      user = User.new(username: username, password: password)
+      if user.save
+        # TODO: Create a token and return it to the client
+        render json: { message: 'User created successfully, redirecting', redirect_url: '/signup/account-selection' }, status: :ok
+      else
+        render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+      end
+    rescue => e
+      render json: { error: `Unexpected error occurred: #{e.message}` }, status: :internal_server_error
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,34 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :password, presence: true
+  # Note_To_Self: This is a Rail method that integrates bcrypt for password encryption.
+  # It automatically provides methods like authenticate and ensures that psasword is hashed and store as
+  # password_digest in the database.
+  # When has_secure_password is set, Rail expects a password_digest column in database, which stores
+  # hashed password. The raw password is still available in memory during validation, so it can be
+  # validated before its encrypted
+  has_secure_password
 
-  def self.validate_username
+  validates :username, presence: true, uniqueness: true, length: { minimum: 10, maximum: 50 }
+  validates :password, presence: true, length: { minimum: 20, maximum: 50 }
+  
+  # Custom validation for the password
+  validate :validate_password
+
+  def self.validate_username(username)
+    return false if username.length < 10 || username.length > 50
+    true
   end
 
-  def self.validate_password
+  def self.validate_password(password)
+    return false unless password.length.between?(20, 50)
+    return false unless password.match?(/[a-zA-Z]/) && password.match?(/\d/)
+    true
+  end
+
+  def validate_password
+    return if password.blank?
+    
+    unless password.match?(/[a-zA-Z]/) && password.match?(/\d/)
+      errors.add(:password,'must contain at least one letter and one number')
+    end
   end
 end
