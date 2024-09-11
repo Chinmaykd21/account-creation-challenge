3b52f63e4b0e05601ae22a34394688fc71d9a5d7
chinmaykd21 <chinmaykd21@gmail.com>
2024-09-10 21:02:49 -0400

    update middleware

--- app/middleware/auth_user.rb
+++ app/middleware/auth_user.rb
@@ -1,73 +1,46 @@
 # app/middleware/auth_user.rb
 class AuthUser
-  SECRET_KEY = ENV["JWT_SECRET_KEY"]
-
   def initialize(app)
     @app = app
   end
 
   def call(env)
     # These paths are public paths
-    skip_paths = ['/create-account', '/api/create-account']
+    public_paths = [
+      '/', 
+      '/api/create-account',
+      '/api/verify-token',
+      '/create-account',
+      '/signup/account-selection',
+      '/signup/create-user',
+      '/signup/deposit',
+      '/signup/joint-access',
+      '/signup/stock-restrictions',
+    ]
 
     request_path = env['PATH_INFO'].downcase
     puts "**** [middleware]: request_path #{request_path}"
 
-    if skip_paths.include?(request_path)
-      puts "**** [middleware]: skipping path"
-      return @app.call(env)
-    end
-
-    puts "**** [Middleware]: Processing request at #{Time.now}"
-    start_time = Time.now
-    
-    token = extract_token(env)
-
-    if token & valid_token?(token)
+    # All paths that do not require user to be authorized are handled here
+    if public_paths.include?(request_path)
+      puts "**** [Middleware]: Public paths resource request processing started at #{Time.now}"
+      start_time = Time.now
       status, headers, response = @app.call(env)
       end_time = Time.now
-      
-      [status, headers, response]
-      puts "**** [Middleware]: Request took #{end_time - start_time} seconds"
-      puts "**** [Middleware]: Response body: #{response.body}" if response.respond_to?(:body)
-    else
-      puts "**** [Middleware]: Processing failed #{Time.now}"
-      return [401, { 'Content-Type' => 'application/json' }, [{
-        error: 'Unauthorized: Invalid or missing token',
-        redirect_url: '/create-account'
-      }.to_json]]
+      puts "**** [Middleware]: Public paths resource request processing ended at #{Time.now}"
+      puts "**** [Middleware]: Public path resource request took #{end_time - start_time} seconds"
+      return [status, headers, response]
     end
-  end
-  
-  private
 
-  def encode_token(payload)
-    JWT.encode(payload, SECRET_KEY) 
-  end
-
-  def decode_token(token)
-    begin
-      decoded_token = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })
-      decoded_token[0]
-    rescue JWT::DecodeError => e
-      Rails.logger.error "JWT Decode Error: #{e.message}"
-      nil
-    end
-  end
-
-  def valid_token?(token)
-    decoded_token = decode_token(token)
-    decoded_token && decoded_token['username']
-    puts "**** [middleware]: decoded token #{decoded_token}"
-  rescue JWT::ExpiredSignature, JWT::DecodeError
-    false
-  end
-
-  def extract_token(env)
-    auth_header = env['HTTP_AUTHORIZATION']
-    return nil unless auth_header
-    puts "**** [middleware]: auth header #{auth_header}"
-    # Assuming the header format is bearer token
-    auth_header.split(' ').last
+    # Future_Enhancement: All protected paths that requires user to be authorized
+    # will be handled here
+    puts "**** [Middleware]: Protected paths resource request processing started at #{Time.now}"
+    start_time = Time.now
+    status, headers, response = @app.call(env)
+    end_time = Time.now
+    puts "**** [Middleware]: Protected paths resource request processing ended at #{Time.now}"
+    puts "**** [Middleware]: Protected path resource request took #{end_time - start_time} seconds"
+    # puts "**** [Middleware]: Response body: #{response.body}" if response.respond_to?(:body)
+    return [status, headers, response]
   end
 end
