21e0295b9929849fe4704f33ba4fa17a3e216078
chinmaykd21 <chinmaykd21@gmail.com>
2024-09-11 19:27:41 -0400

    comment console statements

--- app/middleware/auth_user.rb
+++ app/middleware/auth_user.rb
@@ -19,27 +19,27 @@ class AuthUser
     ]
 
     request_path = env['PATH_INFO'].downcase
-    puts "**** [middleware]: request_path #{request_path}"
+    # puts "**** [middleware]: request_path #{request_path}"
 
     # All paths that do not require user to be authorized are handled here
     if public_paths.include?(request_path)
-      puts "**** [Middleware]: Public paths resource request processing started at #{Time.now}"
-      start_time = Time.now
+      # puts "**** [Middleware]: Public paths resource request processing started at #{Time.now}"
+      # start_time = Time.now
       status, headers, response = @app.call(env)
-      end_time = Time.now
-      puts "**** [Middleware]: Public paths resource request processing ended at #{Time.now}"
-      puts "**** [Middleware]: Public path resource request took #{end_time - start_time} seconds"
+      # end_time = Time.now
+      # puts "**** [Middleware]: Public paths resource request processing ended at #{Time.now}"
+      # puts "**** [Middleware]: Public path resource request took #{end_time - start_time} seconds"
       return [status, headers, response]
     end
 
     # Future_Enhancement: All protected paths that requires user to be authorized
     # will be handled here
-    puts "**** [Middleware]: Protected paths resource request processing started at #{Time.now}"
-    start_time = Time.now
+    # puts "**** [Middleware]: Protected paths resource request processing started at #{Time.now}"
+    # start_time = Time.now
     status, headers, response = @app.call(env)
-    end_time = Time.now
-    puts "**** [Middleware]: Protected paths resource request processing ended at #{Time.now}"
-    puts "**** [Middleware]: Protected path resource request took #{end_time - start_time} seconds"
+    # end_time = Time.now
+    # puts "**** [Middleware]: Protected paths resource request processing ended at #{Time.now}"
+    # puts "**** [Middleware]: Protected path resource request took #{end_time - start_time} seconds"
     # puts "**** [Middleware]: Response body: #{response.body}" if response.respond_to?(:body)
     return [status, headers, response]
   end
