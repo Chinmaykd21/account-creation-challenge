0408ef76b165aea7d756beb798400c4730038cc8
chinmaykd21 <chinmaykd21@gmail.com>
2024-09-12 11:44:39 -0400

    adjust width of account creation form

--- app/frontend/reusable-components/flow-layout/flow-layout.tsx
+++ app/frontend/reusable-components/flow-layout/flow-layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import { Button } from '../button/button';
 import { useNavigate } from 'react-router-dom';
 import { useTokenVerification } from 'app/frontend/hooks/use-token-verification';
+import { twMerge } from 'tailwind-merge';
 
 interface Props {
   children: ReactNode;
@@ -17,8 +18,12 @@ export function FlowLayout({ children }: Props) {
     navigate('/create-account');
   };
 
+  // If the user is not authenticated, any children rendered by flow-layout will have
+  // maximum width of 500px
+  const className = twMerge('h-full mt-5 mx-auto', isAuthenticated ? 'max-w-[1000px]' : 'max-w-[500px]');
+
   return (
-    <div className="h-full mt-5 max-w-[1000px] mx-auto">
+    <div className={className}>
       <div className="w-full text-right">
         {isAuthenticated && (
           <Button classNames="mb-4 rounded-full" onClick={logout}>
