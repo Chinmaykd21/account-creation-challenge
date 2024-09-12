4fb16c8f6bd01ec3bf5cf4a38444e61de8db59e6
chinmaykd21 <chinmaykd21@gmail.com>
2024-09-12 11:45:16 -0400

    add gap between image, title, description fields on card component

--- app/frontend/reusable-components/card/card.tsx
+++ app/frontend/reusable-components/card/card.tsx
@@ -11,14 +11,15 @@ interface Props {
 export function Card({ children, title, description, showWealthFrontLogo }: Props) {
   return (
     <section className="p-10 shadow-card min-h-[400px] w-full rounded-2xl border border-solid border-slate-200">
-      {/* TODO: Create a general image component */}
-      {showWealthFrontLogo && (
-        <div className="flex w-full justify-center">
-          <WealthfrontLogo width={50} height={50} />
-        </div>
-      )}
-      <h1 className="text-2xl text-center font-bold m-0 mb-1">{title}</h1>
-      <p className="text-[hsla(243,30%,13%,.63)] text-base m-0 mb-1">{description}</p>
+      <div className="space-y-4">
+        {showWealthFrontLogo && (
+          <div className="flex w-full justify-center">
+            <WealthfrontLogo width={50} height={50} />
+          </div>
+        )}
+        <h1 className="text-2xl text-center font-bold m-0 mb-1">{title}</h1>
+        <p className="text-[hsla(243,30%,13%,.63)] text-base m-0 mb-1">{description}</p>
+      </div>
       {children}
     </section>
   );
